daea359866f88d24bd0ee9c0a7e7d272af31491f
WistitiPNF-404 <guilmario26@hotmail.com>
2026-01-20 23:09:52 -0500

    2026-01-20 Project changes

--- src/reload.lua
+++ src/reload.lua
@@ -18,19 +18,22 @@ end
 
 function prefix_SetupMap()
 	print('Map is loading, here we might load some packages.')
-	-- LoadPackages({Name = package_name_string})
+    LoadPackages({ Name = "BiomeP" })
+	LoadPackages({ Name = "Fx" })
+	mod.LoadBoonFramesPackage()
 end
 
-function trigger_Gift()
-	modutil.mod.Hades.PrintOverhead(config.message)
-end
+--[[function trigger_Gift()
+	-- modutil.mod.Hades.PrintOverhead(config.message)
+	modutil.mod.Hades.PrintOverhead("Comme les 5 doigts de la main")
+end]]--
 
 
 -------------------------------------------------------------------
 -- This function is part of the mod creation guide from the wiki --
 -------------------------------------------------------------------
-function mod.LoadSkellyPackage()
-	local packageName = _PLUGIN.guid .. "Portraits"
-	print("AuthorName-ModName - Loading package: " .. packageName)
+function mod.LoadBoonFramesPackage()
+	local packageName = _PLUGIN.guid .. "BoonFrames"
+	print("Wistiti-WrathOfOlympus - Loading package: " .. packageName)
 	LoadPackages({ Name = packageName })
 end
